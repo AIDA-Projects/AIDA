e15a2dfa769a293bf3ed66e3bff316f1067e3ed3
真心 <ceo@iqianye.cn>
2021-08-09 23:20:34 +0800

    Convert to java

---
+++ app/src/main/java/me/zhenxin/aida/activity/BaseActivity.java
@@ -0,0 +1,20 @@
+package me.zhenxin.aida.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Basic Activity
+ *
+ * @author ZhenXin
+ * @email qgzhenxin@qq.com
+ * @since 2021/8/9 20:43
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
