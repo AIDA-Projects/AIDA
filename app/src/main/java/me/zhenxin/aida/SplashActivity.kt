2080ede5b36897b6fe6c8b523bf823d023cb2111
真心 <ceo@iqianye.cn>
2021-08-09 20:59:01 +0800

    Change minSdk version to Android 5 (21)

--- app/src/main/java/me/zhenxin/aida/SplashActivity.kt
+++ app/src/main/java/me/zhenxin/aida/SplashActivity.kt
@@ -1,9 +1,16 @@
 package me.zhenxin.aida
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.zhenxin.aida.activity.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+/**
+ * Splash Activity
+ *
+ * @author ZhenXin
+ * @since 2021/8/9 17:18
+ * @email qgzhenxin@qq.com
+ */
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
