ef1f9f9e73b9df2d0dbd9370ab874ed6d0623eb1
真心 <ceo@iqianye.cn>
2021-08-13 20:47:37 +0800

    fix

--- app/src/main/kotlin/me/zhenxin/aida/activity/WelcomeActivity.kt
+++ app/src/main/kotlin/me/zhenxin/aida/activity/WelcomeActivity.kt
@@ -1,10 +1,9 @@
 package me.zhenxin.aida.activity
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import me.zhenxin.aida.databinding.ActivityWelcomeBinding
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
     private lateinit var binding: ActivityWelcomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
